9182790c5f6f2705c925fc884223d8d4f0fcb86e
mseidel819 <mseidel819@gmail.com>
2022-04-19 09:00:29 -0500

    removed the crap that onedrive added....that pos.... added shop pages. stored data in firebase

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { CustomButtonContainer } from "./customButton.styles";
-
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-);
-
-export default CustomButton;
